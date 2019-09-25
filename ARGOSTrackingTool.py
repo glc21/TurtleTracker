48a97e99196545baef2137eba7e136968aca3ef2
glc21 <glc21@duke.edu>
2019-09-25 12:59:06 -0400

    Initial commit of ARGOSTrackingTool script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Grace Cullinan (glc21@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
